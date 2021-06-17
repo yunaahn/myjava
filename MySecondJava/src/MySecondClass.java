dcb9c9546e0ea0b782199aeda3569fa8401be8a0
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 21:50:10 +0900

    커밋 ㅇㅇㅇ

--- MySecondJava/src/MySecondClass.java
+++ MySecondJava/src/MySecondClass.java
@@ -12,6 +12,8 @@ public class MySecondClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("hello worlㅇㅇㅇㅇd");
+		System.out.println("hello world");
 		System.out.println("hello world");
 		System.out.println("hello world");
 	}
