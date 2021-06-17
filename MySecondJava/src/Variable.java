2d8ca0f855dff0acf631557dbd6f82f816c1b667
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 21:58:41 +0900

    변수 실습

---
+++ MySecondJava/src/Variable.java
@@ -0,0 +1,15 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int mynumber; 
+		mynumber = 10;
+		
+		int mynumber2 = 10;
+		
+		System.out.println("mynumber의 값은"+mynumber);
+		System.out.println("mynumber2의 값은"+mynumber2);
+
+	}
+
+}
