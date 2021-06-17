1df6adfe203b4c6b63e80e2dda5b981c29edb15b
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 21:40:39 +0900

    커밋 1244523

---
+++ MySecondJava/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"MySecondClass","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"MySecondClass","l":"MySecondClass()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ MySecondJava/src/MySecondClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author TJ
+ *
+ */
+
+public class MySecondClass {
+/**
+ * 
+ * @param project > generate java doc 
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+	}
+
+}
