447e8dc6aaf5baa72a28e7aa1a25dda2ffc27418
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 22:05:47 +0900

    변수

--- MySecondJava/src/Variable.java
+++ MySecondJava/src/Variable.java
@@ -9,6 +9,14 @@ public class Variable {
 		
 		System.out.println("mynumber의 값은"+mynumber);
 		System.out.println("mynumber2의 값은"+mynumber2);
+		
+		String name = "홍길동";
+		String name2 = null;
+		String name3 = "";
+		
+		System.out.println("name의 값은"+name);
+		System.out.println("name의 값은"+name2);
+		System.out.println("name의 값은"+name3);
 
 	}
 
